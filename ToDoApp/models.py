30a264b7750774343d03ac4faed1e470a925c031
Binu Aliyas <binu.aliyas@gmail.com>
2020-07-31 12:41:20 -0400

    date feature added

--- ToDoApp/models.py
+++ ToDoApp/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    text=models.CharField(max_length=100)
+    text=models.CharField(max_length=60)
     complete=models.BooleanField(default=False)
+    date_added=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
